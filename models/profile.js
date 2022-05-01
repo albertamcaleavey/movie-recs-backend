61ada519a7fa3fc164c07b182b9acff23512fc95
Alberta McAleavey <a.mcaleavey24@gmail.com>
2022-05-01 16:46:12 -0400

    create db collection and embed task schema in profile schema

--- models/profile.js
+++ models/profile.js
@@ -1,11 +1,19 @@
 import mongoose from 'mongoose'
 
+const taskSchema = new mongoose.Schema({
+  description: {type: String, required: true}
+}, {
+  timestamps: true
+})
+
 const profileSchema = new mongoose.Schema({
   email: {type: String, required: true, lowercase: true, unique: true},
   name: String,
+  tasks: [taskSchema]
 },{
     timestamps: true,
 })
+
 
 const Profile = mongoose.model('Profile', profileSchema)
 
